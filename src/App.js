ff2c9c55ba2e7885c4f8e8e73dd27a68e0eb4630
Bingcheng Ni <nibingcheng@johndeere.com>
2021-03-13 12:06:17 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0
+    };
+  }
+  
+  increaseCount = () => {
+    let count = this.state.count + 1;
+    this.setState({
+      count: count
+    });
+  };
+
+  decreaseCount = () => {
+    let count = this.state.count - 1;
+    this.setState({
+      count: count
+    });
+  };
+
+  render() {
+    // console.log(this.state);
+    return (
+      <div className="App">
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.increaseCount}>+</button>
+        <button onClick={this.decreaseCount}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
